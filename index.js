2d64a1281f056fc9b2ca9f1473b6208a75caf27e
Hyebin-Park <foreverinthesun.daum.net>
2019-02-28 21:59:20 +0900

    Express Core Middlewares part Two

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //const express = require('express');
 
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -12,13 +17,15 @@ const handleHome = (req, res) => res.send('hello from home');
 
 const handleProfile = (req, res) => res.send('you are on my profile');
 
-const betweenHome = (req, res,next) => {
-    console.log("i'm between");
-    next();
-}
 
 //use middleware globally for all route
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
