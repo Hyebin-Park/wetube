5b85e0f41b1cf063207bddacc31e0b284c18973f
Hyebin-Park <foreverinthesun.daum.net>
2019-03-14 19:46:02 +0900

    Local and Template Variables in Pug

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import globalRouter from "./routers/globalRouter";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 const app = express();
 
+// HTTP 헤더를 적절히 설정하여 웹 취약성으로부터 앱을 보호하는 미들웨어
+app.use(helmet());
 
 // pug : html 파일 작성시 반복 작업을 최소화 하게 해주는 템플릿 엔진 중 하나
 app.set('view engine', "pug")
@@ -23,11 +26,10 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
-// HTTP 헤더를 적절히 설정하여 웹 취약성으로부터 앱을 보호하는 미들웨어
-app.use(helmet());
-
 // application에서 발생하는 모든 일들을 logging 하는 미들웨어
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
