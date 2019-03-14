e93070811242140848345d8b5d5e045c07e5f359
Hyebin-Park <foreverinthesun.daum.net>
2019-03-14 21:03:18 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,14 @@
 // res.render 함수에 들어가는 인자 1 : 템플릿 / 인자 2 : 템플릿에 추가할 정보가 담긴 객체
 export const home = (req, res) => res.render("home", { pageTitle : "Home" });
-export const search = (req, res) => res.render("Search", { pageTitle : "Search" });
+
+export const search = (req, res) => { 
+    // const searchingBy = req.query.term <-- OLD SCHOOL WAY
+    // USING Destructuring assignment <-- NEW SCHOOL ES6 WAY reference : https://medium.com/@pyrolistical/destructuring-nested-objects-9dabdd01a3b8
+    console.log(req.query.term);
+    const { query: { term: searchingBy } } = req;
+    res.render("Search", { pageTitle : "Search", searchingBy})
+};
+
 export const videos = (req, res) => res.render("videos", { pageTitle : "Videos" })
 export const upload = (req, res) => res.render("upload", { pageTitle : "Upload" })
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "Video Detail" })
