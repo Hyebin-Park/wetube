2d8f4e0aa556f0e70f3e4bf0dd05791d85a0c246
Hyebin-Park <foreverinthesun.daum.net>
2019-03-16 20:39:39 +0900

    Change routes has variable, Log In and User Profile Controller

--- middlewares.js
+++ middlewares.js
@@ -4,5 +4,9 @@ export const localsMiddleware = (req, res, next) => {
     // res.locals : 뷰를 렌더링하는 기본 콘텍스트를 포함하는 객체로, 템플릿에서 접근 가능한 변수들을 담고있다.
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true,
+        id: 1
+    };
     next();
 }
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,9 +6,11 @@ const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
 
+// 여기 속하는 주소는 모두 /user/~
+
 // 라우터가 /edit-profile을 :id로 인식하기 때문에 순서를 바꿔준다.
+userRouter.get(routes.userDetail(), userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
