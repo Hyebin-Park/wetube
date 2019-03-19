49d50c05a675ceb4db6aa3789351957c010e4fa6
Hyebin-Park <foreverinthesun.daum.net>
2019-03-19 13:26:44 +0900

    Comment Model

---
+++ models/Video.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+// todo : model(document name, actual data) / schema(shape, definition)
+
+const VideoSchema = new mongoose.Schema({
+    fileUrl: {
+        type: String,
+        required: "File URL is required"
+    },
+    title: {
+        type: String,
+        required: "Title is required"
+    },
+    description: String,
+    views: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    // comment의 모든 정보를 저장하는 대신, Comment의 id를 배열에 저장하는 방법.
+    // comments: [
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "Comment"
+    //     }
+    // ]
+
+})
+
+const model = mongoose.model("Video", VideoSchema);
+export default model;
