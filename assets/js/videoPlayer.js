e76ecdee5007ca7fdd4c87e63f84564037b1c2e1
Hyebin-Park <foreverinthesun.daum.net>
2019-03-30 17:34:44 +0900

    Mute / Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,6 +1,7 @@
 const videoContainer = document.getElementById('jsVideoPlayer');
 const videoplayer = document.querySelector('#jsVideoPlayer video');
 const playBtn = document.getElementById('jsVideoPlayBtn');
+const volumeBtn = document.getElementById('jsVolumeBtn');
 
 const handlePlayClick = () => {
     if(videoplayer.paused){
@@ -10,8 +11,21 @@ const handlePlayClick = () => {
     }
 }
 
+const handleVolumeClick = () => {
+    if(videoplayer.muted){
+        videoplayer.muted = false;
+        volumeBtn.innerHTML = `<i class="fas fa-volume-up"></i>`
+    } else {
+        if(!videoplayer.muted) {
+            videoplayer.muted = true;
+            volumeBtn.innerHTML = `<i class="fas fa-volume-mute"></i>`        
+        }
+    }
+}
+
 const init = () => {
     playBtn.addEventListener("click", handlePlayClick);
+    volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 if(videoContainer){
